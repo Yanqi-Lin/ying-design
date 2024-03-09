221175fcf46f1c6658b2ad450500b295a7b04a28
erdong-js <zhiguangphoenix@gmail.com>
2024-03-09 19:39:27 +0800

    feat: 修改Icon.stories.ts中的ts类型报错

--- src/components/Icon/Icon.stories.ts
+++ src/components/Icon/Icon.stories.ts
@@ -1,8 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Icon from "./index";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
-const meta = {
-  title: "Component/Button",
+const meta: Meta = {
+  title: "Component/Icon",
   component: Icon,
   parameters: {
     layout: "centered",
@@ -16,6 +16,6 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     icon: "primary",
-    children: "Primary",
+    children: "Primary", // 我看Icon的类型定义里面没有显式的声明children属性？虽然没报错，但是确定这样写可以吗？
   },
 };

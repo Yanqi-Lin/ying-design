32e7287f1e73727b93dce2e1e4a1ef0aafe54f96
Yanqi-Lin <664506986@qq.com>
2024-03-14 09:14:38 +0800

    edit Icon storybook

--- src/components/Icon/Icon.stories.ts
+++ src/components/Icon/Icon.stories.ts
@@ -1,13 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Icon from "./index";
-<<<<<<< HEAD
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 library.add(fas);
 
-=======
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
->>>>>>> ae7b7aa0b649ec54b577635c64db98616baecd80
 const meta: Meta = {
   title: "Component/Icon",
   component: Icon,
@@ -22,11 +18,15 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-<<<<<<< HEAD
     icon: "check",
-=======
-    icon: "coffee",
->>>>>>> ae7b7aa0b649ec54b577635c64db98616baecd80
     theme: "primary",
+  },
+};
+
+export const Large: Story = {
+  args: {
+    icon: "check",
+    theme: "primary",
+    size: "3x",
   },
 };

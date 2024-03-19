e9ca1fb1df09989d755ad3558068941b80c2ed5c
Yanqi-Lin <664506986@qq.com>
2024-03-19 15:29:02 +0800

    add storybook

--- dist/components/Icon/Icon.stories.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Meta, StoryObj } from "@storybook/react";
-declare const meta: Meta;
-export default meta;
-type Story = StoryObj<typeof meta>;
-export declare const Primary: Story;
-export declare const Large: Story;
